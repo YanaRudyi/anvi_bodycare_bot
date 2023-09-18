a76591b91d4e3df992c9de720c13f953d69a4011
SoundsGreaat <imithos321@gmail.com>
2023-09-18 15:38:03 +0300

    pytests

---
+++ tests/test_handlers/test_start.py
@@ -0,0 +1,13 @@
+from unittest.mock import AsyncMock
+
+import pytest
+
+from bot import send_main_menu, main_menu_keyboard
+
+
+@pytest.mark.asyncio
+async def test_start_handler():
+    message = AsyncMock()
+    await send_main_menu(message)
+    message.answer.assert_called_with(message.chat.id, "Ласкаво просимо в Anvi! Як ми можемо допомогти вам сьогодні?",
+                                      reply_markup=main_menu_keyboard)
