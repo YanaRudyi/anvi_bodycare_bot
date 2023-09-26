e4efbace263c145f43af078de3714878707507f0
YanaRudyi <89380889+YanaRudyi@users.noreply.github.com>
2023-09-26 21:57:52 +0200

    [database_setup] change database_setup

--- database_setup.py
+++ database_setup.py
@@ -21,23 +21,9 @@ def close_connection(conn):
         print('Database connection closed.')
 
 
-def create_table(conn):
-    try:
-        cur = conn.cursor()
-        cur.execute(
-            "CREATE TABLE IF NOT EXISTS question (id serial PRIMARY KEY, name varchar, phone_number varchar, "
-            "question varchar);")
-        conn.commit()
-        cur.close()
-        print('Table "question" created successfully.')
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(f"Error creating table: {error}")
-
-
 def main():
     conn = connect()
     if conn is not None:
-        create_table(conn)
         close_connection(conn)
 
 
