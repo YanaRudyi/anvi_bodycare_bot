091d0b7313f9f32cb908cda37b34c453c2f16d55
YanaRudyi <89380889+YanaRudyi@users.noreply.github.com>
2023-10-06 01:16:30 +0200

    [parser_product_page] remove redundant files and data

--- model.py
+++ model.py
@@ -4,13 +4,3 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 metadata = Base.metadata
-
-
-class Question(Base):
-    """Represents a question in the database."""
-    __tablename__ = 'question'
-
-    id = Column(INTEGER, primary_key=True, autoincrement=True)
-    name = Column(String(80), unique=False, nullable=False)
-    phone_number = Column(String(120), unique=False, nullable=False)
-    question = Column(String(120))
