c3a16cc33c463e8cc90c20e55b84b8788c830c77
SoundsGreaat <imithos321@gmail.com>
2023-10-06 17:03:32 +0300

    price display fix

--- catalogue_functions.py
+++ catalogue_functions.py
@@ -7,11 +7,20 @@ def send_product_info(product_index):
     product_data = parse_product_page(get_product_page_links(shop_url)[product_index])
     product_name = product_data.get('product name', 'N/A')
     description = product_data.get('description', 'N/A')
-    price = product_data.get('prices', ['N/A'])[0]
-
     message = f"{product_name}\n\n"
-    message += f"Опис: {description}\n\n"
-    message += f"Ціна: {price}\n"
+    if description:
+        message += f"Опис: {description}\n\n"
+    try:
+        prices = product_data.get('price', ['N/A'])[1:]
+
+        min_price = min(int(price[2]) for price in prices)
+        max_price = max(int(price[2]) for price in prices)
+
+        message += f"Ціна: {min_price} - {max_price}₴\n"
+
+    except ValueError:
+        prices = product_data.get('price', ['N/A'])[0]
+        message += f"Ціна: {prices}\n"
 
     return message
 
